01f90068f22b0cad4d8ae2a6634bcab09174e320
Carlos Duque <carlosduque.hn@gmail.com>
2020-06-23 23:00:54 -0400

    implemented the latch

--- threads/src/main/java/o/oo/threads/MainLatch.java
+++ threads/src/main/java/o/oo/threads/MainLatch.java
@@ -10,45 +10,20 @@ import java.util.concurrent.Future;
 import java.util.concurrent.CountDownLatch;
 
 public class MainLatch {
-    public static void main(String[] args) throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(2);
-        CountDownLatch latch = new CountDownLatch(2);
-        List<Future> fs = new ArrayList<Future>();
+    public static void main(final String[] args) throws Exception {
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
+        final CountDownLatch latch = new CountDownLatch(2);
+        final List<Future> fs = new ArrayList<Future>();
+
+        fs.add(executor.submit(new RequiredForLatch(latch)));
         for (int i = 0; i < 9; i++) {
-            fs.add(executor.submit(new Normal(latch)));
-            if (7 == i) fs.add(executor.submit(new Required(latch)));
+            fs.add(executor.submit(new NormalForLatch(latch)));
         }
         System.out.println("sending last required task");
-        fs.add(executor.submit(new Required(latch)));
-        fs.add(executor.submit(new Required(latch)));
-        fs.add(executor.submit(new Required(latch)));
-        for (Future f : fs)
+        latch.countDown();
+
+        for (final Future f : fs)
             System.out.println(f.get());
     }
 
-    public static class Required implements Callable<String> {
-        private CountDownLatch latch;
-        public Required(CountDownLatch latch) {
-            this.latch = latch;
-        }
-        public String call() throws Exception {
-            String str = Thread.currentThread().getName() + "::" + System.currentTimeMillis() + "::Required:" + UUID.randomUUID();
-            System.out.println(str);
-            latch.countDown();
-            return str + ":count#" + latch.getCount();
-        }
-    }
-
-    public static class Normal implements Callable<String> {
-        private CountDownLatch latch;
-        public Normal(CountDownLatch latch) {
-            this.latch = latch;
-        }
-        public String call() throws Exception {
-            String str = Thread.currentThread().getName() + "::" + System.currentTimeMillis() + "::Normal:";
-            System.out.println(str + "awaiting... #" + latch.getCount());
-            latch.await();
-            return str + UUID.randomUUID();
-        }
-    }
 }
